c17bffb05b7489abdebe3ce5d641beaf6de0dd12
TheMatrixShibe <TheMatrixShibe@users.noreply.github.com>
2015-08-21 08:30:41 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@ if npm; then
   echo npm exists
 else 
   echo npm does not exist.
+  exit
 fi
 if node -v; then
   echo node exists
 else 
-  echo node does not exist
+  echo node does not exist.
+  exit
 fi
 npm install colors
 npm install big-integer
