2007630843cce4e4ab496eab958750e20b7e2e49
TheMatrixShibe <TheMatrixShibe@users.noreply.github.com>
2015-08-20 12:44:11 +1000

    Here we are, a proper keyboard!
    Now to make sure I don't get crumbs over a school laptop.
    
    Sandwiches. Ugh.

--- main.js
+++ main.js
@@ -6,10 +6,26 @@ var requires = {
 };
 
 console.log("Blockchain Expiriment ".blue.bold, "node version ", "0.0.1".green.bold);
-console.log("~~[  init  ]~~".red.dim);
+console.log("~~[  init  ]~~".rainbow.dim);
 
 
 //These have to be executed before the node can start.
 // Files to be loaded include block and node lists.
 
-console.log("Checking if block file exists. . .");
+console.log("Checking if block folder exists. . .");
+
+try {
+    // Query the entry
+    stats = requires.fs.lstatSync('./blocks');
+    // Is it a directory?
+    if (stats.isDirectory()) {
+      console.log("Block folder exists.".green);
+    } else {
+      console.log("Block folder is a file :/ Fixing".yellow);
+      requires.fs.mkDirSync('./blocks');
+    }
+} catch (e) {
+    console.log("Block folder does not exist. Creating.".red.bold);
+    console.log("If this is your first time executing the node program, ignore this.");
+    requires.fs.mkDirSync('./blocks');
+}
